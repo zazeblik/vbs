3cc8db958ff9b65318eac2e2f9d78639afecdb51
zazeblik <zazeblik@mail.ru>
2020-10-23 22:36:03 +0300

    feat: Проверка на наличие участников и групп при создании архивной записи

--- frontend/src/views/database/ArchivePersons.vue
+++ frontend/src/views/database/ArchivePersons.vue
@@ -1,7 +1,12 @@
 <template>
   <b-container fluid class="py-2">
     <h5>Участники в архиве</h5>
-    <DataTable :baseUrl="baseUrl" :fields="fields" :itemForm="itemForm" filterPlaceHolder="Введите имя..." />
+    <DataTable 
+      :baseUrl="baseUrl" 
+      :fields="fields" 
+      :itemForm="itemForm" 
+      filterPlaceHolder="Введите имя..." 
+      :creationErrorMessage="creationErrorMessage" /> 
   </b-container>
 </template>
 
@@ -13,6 +18,7 @@ export default {
     return {
       baseUrl: "/archivepersons",
       itemForm: ArchivePersonForm,
+      creationErrorMessage: null,
       fields: [
         {
           key: "person",
@@ -44,6 +50,9 @@ export default {
       const settings = await this.$getAsync(`${this.baseUrl}/settings`);
       this.itemForm.find(f => f.property == "person").models = settings.persons;
       this.itemForm.find(f => f.property == "group").models = settings.groups;
+      if (settings.persons.length == 0 || settings.groups.length == 0) {
+        this.creationErrorMessage = "Для создания записи необходимо создать хотябы одного участника и хотябы одну группу";
+      }
     },
   }
 };
