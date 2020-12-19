44c0b83e67d54025aca0515315f241cae27b77f3
zazeblik <zazeblik@mail.ru>
2020-12-20 01:47:27 +0300

    fix: Ещё косяк с объявлением

--- api/utils/DateUtil.js
+++ api/utils/DateUtil.js
@@ -1,5 +1,5 @@
-const timeZoneOffset = sails.config.tz ? -1 * sails.config.tz : 0;
 module.exports.GetDate = function (year = 0, month = 0, date = 0, hours = 0, minutes = 0, seconds = 0, miliseconds = 0){
+  const timeZoneOffset = sails.config.tz ? -1 * sails.config.tz : 0;
   let d = year ? new Date(year, month, date, hours, minutes, seconds, miliseconds) : new Date();
   d.setHours(d.getHours() + timeZoneOffset);
   return d;
